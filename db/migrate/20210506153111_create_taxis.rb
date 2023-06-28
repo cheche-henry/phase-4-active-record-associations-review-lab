918141487c1febff99ba0de4c25141835cdadad3
henry cheche <henry.cheche@student.moringaschool.com>
2023-06-28 22:51:52 +0300

    done

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,6 +1,7 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
+      # Add any additional columns for the taxis table if needed
 
       t.timestamps
     end
